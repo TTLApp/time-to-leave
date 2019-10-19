b22a0900b46e523c5f17309374d77bac3d6a8968
Nicholas Browning <shmolf@gmail.com>
2019-10-19 02:46:31 -0400

    Themes: Added checks for globally defined modules, before re-defining

--- js/theme_init.js
+++ js/theme_init.js
@@ -1,5 +1,11 @@
 'use strict';
 
+if (typeof ipcRenderer !== 'undefined') {
+    const { ipcRenderer } = require('electron');
+}
+if (typeof getUserPreferences !== 'undefined') {
+    const { getUserPreferences } = require('./js/UserPreferences.js');
+}
 const { applyTheme } = require('./js/Themes.js');
 
 ipcRenderer.on('PREFERENCE_SAVED', function (event, inputs) {
