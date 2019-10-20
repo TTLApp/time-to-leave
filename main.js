5cc9d4eee932204c69d06bcc3840410b4cafd874
Kashaiahyah85 <kashaiahyah85@icloud.com>
2019-10-19 22:02:08 -0700

    [enhancement] require('update-electron-app')() #43 	modified:   main.js

--- main.js
+++ main.js
@@ -231,3 +231,7 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+require('update-electron-app')({
+    repo: 'thamara/time-to-leave',
+    updateInterval: '1 day'
+});
