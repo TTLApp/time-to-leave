90d87ef1bc0d69b4490a495c13b8cdb685e82799
Thamara Andrade <tkcandrade@gmail.com>
2020-10-10 20:40:08 -0300

    Adding docstrings

--- js/date-to-string-util.js
+++ js/date-to-string-util.js
@@ -1,11 +1,19 @@
 const dayAbbrs = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
 const monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
 
+/**
+ * @param dayIndex Week day index, considering a week starting on sunday
+ * @return The abbreviation (3-letters) for the week day
+ */
 function getDayAbbr(dayIndex)
 {
     return dayAbbrs[dayIndex];
 }
 
+/**
+ * @param monthIndex Month index, considering 0 as January
+ * @return The month name for the passed index
+ */
 function getMonthName(monthIndex)
 {
     return monthNames[monthIndex];
