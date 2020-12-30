d7db1a17ea1c64d5e02a86e91112c91789c1f45d
Jose Navarro <josenavs22@gmail.com>
2020-12-29 17:38:06 -0700

    Delete unnecessary comments, fix eslint errors and make function only work when the OS is Windows

--- main.js
+++ main.js
@@ -7,13 +7,16 @@ const { notify } = require('./js/notification');
 const { getUserPreferences } = require('./js/user-preferences.js');
 const i18n = require('./src/configs/i18next.config');
 const { handleSquirrelEvent } = require('./js/squirrel.js');
+const { appConfig } = require('./js/app-config');
 
-/* eslint-disable */
-if (handleSquirrelEvent(app)) {
-    // squirrel event handled and app will exit in 1000ms, so don't do anything else
-    return;
-  }
-/* eslint-enable */
+if (appConfig.win32)
+{
+    if (handleSquirrelEvent(app))
+    {
+        // squirrel event handled and app will exit in 1000ms, so don't do anything else
+        return;
+    }
+}
 
 i18n.on('loaded', () =>
 {
