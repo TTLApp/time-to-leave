635a2501b33b112dcc2fb1fd2759068cdef7364b
fcoiuri <fcoiuri@gmail.com>
2021-10-14 18:25:51 -0300

    Add #772 second test and done

--- __tests__/__main__/date-aux.js
+++ __tests__/__main__/date-aux.js
@@ -6,11 +6,15 @@ const { getDateStr, getCurrentDateTimeStr } = require('../../js/date-aux');
 describe('Date Functions', () =>
 {
     const badDate = ['this', 'is', 'not', 'a', 'date'];
+
     const testDate = new Date();
     const expectedDate = new Date(testDate.getTime() - (testDate.getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
     const testCurrentTimeStr = testDate.toLocaleTimeString([], {hour: '2-digit', hourCycle: 'h23', minute:'2-digit', second:'2-digit'}).substr(0, 8);
     const reg = /[-:]/g;
     const testGetCurrentDateTimeStr = `${expectedDate}_${testCurrentTimeStr}`.replace(reg,'_');
+    const regFullDate = /[0-9]{4}_(0[1-9]|1[0-2])_(0[1-9]|[1-2][0-9]|3[0-1])_(2[0-3]|[01][0-9])_[0-5][0-9]_[0-5][0-9]/g;
+    const testNoSpaceUnCha = regFullDate.test(testGetCurrentDateTimeStr);
+
 
 
 
@@ -35,6 +39,10 @@ describe('Date Functions', () =>
         {
             expect(getCurrentDateTimeStr()).toBe(testGetCurrentDateTimeStr);
         });
+        test('Test that there are no spaces or unexpected characters', () =>
+        {
+            expect(testNoSpaceUnCha).toBeTruthy();
+        })
     });
 
 });
