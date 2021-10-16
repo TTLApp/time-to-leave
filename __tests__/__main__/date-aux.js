52d19230ed900b5bbc670b06ce9b558366746073
fcoiuri <fcoiuri@gmail.com>
2021-10-16 20:26:44 -0300

    check if the format is the expected with the regular expression

--- __tests__/__main__/date-aux.js
+++ __tests__/__main__/date-aux.js
@@ -6,12 +6,8 @@ const { getDateStr, getCurrentDateTimeStr } = require('../../js/date-aux');
 describe('Date Functions', () =>
 {
     const badDate = ['this', 'is', 'not', 'a', 'date'];
-
     const testDate = new Date();
     const expectedDate = new Date(testDate.getTime() - (testDate.getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
-    const testCurrentTimeStr = testDate.toLocaleTimeString([], {hour: '2-digit', hourCycle: 'h23', minute:'2-digit', second:'2-digit'}).substr(0, 8);
-    const reg = /[-:]/g;
-    const testGetCurrentDateTimeStr = `${expectedDate}_${testCurrentTimeStr}`.replace(reg,'_');
     
     describe('getDateStr(Date())', () =>
     {
@@ -30,18 +26,11 @@ describe('Date Functions', () =>
     describe('getCurrentDateTimeStr()', () =>
     {
         const regFullDate = /[0-9]{4}_(0[1-9]|1[0-2])_(0[1-9]|[1-2][0-9]|3[0-1])_(2[0-3]|[01][0-9])_[0-5][0-9]_[0-5][0-9]/g;
-        const testNoSpaceUnCha = regFullDate.test(testGetCurrentDateTimeStr);
-        //Test the format of the output using regular expression (The format is YYYY_MM_DD_HH_MM_SS)
-        test('Test the format of the output using regular expression', () =>
+        const testFormatNoSpace = regFullDate.test(getCurrentDateTimeStr());
+        test('Test the format of the output using regular expression and there are no spaces or unexpected characters', () =>
         {
-            expect(getCurrentDateTimeStr()).toBe(testGetCurrentDateTimeStr);
-        });
-        test('Test that there are no spaces or unexpected characters', () =>
-        {
-            expect(testNoSpaceUnCha).toBeTruthy();
+            expect(testFormatNoSpace).toBeTruthy();
         });
     });
 
 });
-
-
