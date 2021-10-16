17fe492ae59ccdb9f0468c8fdf7e36313129d9a5
fcoiuri <fcoiuri@gmail.com>
2021-10-16 19:58:54 -0300

    removing empty lines and moving consts to scope of the test

--- __tests__/__main__/date-aux.js
+++ __tests__/__main__/date-aux.js
@@ -12,37 +12,34 @@ describe('Date Functions', () =>
     const testCurrentTimeStr = testDate.toLocaleTimeString([], {hour: '2-digit', hourCycle: 'h23', minute:'2-digit', second:'2-digit'}).substr(0, 8);
     const reg = /[-:]/g;
     const testGetCurrentDateTimeStr = `${expectedDate}_${testCurrentTimeStr}`.replace(reg,'_');
-    const regFullDate = /[0-9]{4}_(0[1-9]|1[0-2])_(0[1-9]|[1-2][0-9]|3[0-1])_(2[0-3]|[01][0-9])_[0-5][0-9]_[0-5][0-9]/g;
-    const testNoSpaceUnCha = regFullDate.test(testGetCurrentDateTimeStr);
-
-
-
-
-
+    
     describe('getDateStr(Date())', () =>
     {
         test('Given a JS Date() object, should return YYYY-MM-DD', () =>
         {
             expect(getDateStr(testDate)).toBe(expectedDate);
         });
-
+        
         test('Given an insane object, should return an error', () =>
         {
             expect(getDateStr(badDate)).not.toBe(expectedDate);
         });
-
+        
     });
-
+    
     describe('getCurrentDateTimeStr()', () =>
     {
-        test('Test the format of the output using regular expression (The format is YYYY_MM_DD_HH_MM_SS)', () =>
+        const regFullDate = /[0-9]{4}_(0[1-9]|1[0-2])_(0[1-9]|[1-2][0-9]|3[0-1])_(2[0-3]|[01][0-9])_[0-5][0-9]_[0-5][0-9]/g;
+        const testNoSpaceUnCha = regFullDate.test(testGetCurrentDateTimeStr);
+        //Test the format of the output using regular expression (The format is YYYY_MM_DD_HH_MM_SS)
+        test('Test the format of the output using regular expression', () =>
         {
             expect(getCurrentDateTimeStr()).toBe(testGetCurrentDateTimeStr);
         });
         test('Test that there are no spaces or unexpected characters', () =>
         {
             expect(testNoSpaceUnCha).toBeTruthy();
-        })
+        });
     });
 
 });
