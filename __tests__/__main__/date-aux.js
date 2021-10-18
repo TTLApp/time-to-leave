e8e420a259f81b5dd1919fa5718a10c0c346a953
Vamsi Charan Adari <adarivamsi@gmail.com>
2021-10-17 23:20:49 -0500

    Added previous changes.

--- __tests__/__main__/date-aux.js
+++ __tests__/__main__/date-aux.js
@@ -1,31 +1,37 @@
 /* eslint-disable no-undef */
-"use strict";
+'use strict';
 
-const { getDateStr, getCurrentDateTimeStr } = require("../../js/date-aux");
+const { getDateStr, getCurrentDateTimeStr } = require('../../js/date-aux');
 
-describe("Date Functions", () => {
-  const badDate = ["this", "is", "not", "a", "date"];
-  const testDate = new Date();
-  const expectedDate = new Date(
-    testDate.getTime() - testDate.getTimezoneOffset() * 60000
-  )
-    .toISOString()
-    .substr(0, 10);
+describe('Date Functions', () =>
+{
+    const badDate = ['this', 'is', 'not', 'a', 'date'];
+    const testDate = new Date();
+    const expectedDate = new Date(
+        testDate.getTime() - testDate.getTimezoneOffset() * 60000
+    )
+        .toISOString()
+        .substr(0, 10);
 
-  describe("getDateStr(Date())", () => {
-    test("Given a JS Date() object, should return YYYY-MM-DD", () => {
-      expect(getDateStr(testDate)).toBe(expectedDate);
+    describe('getDateStr(Date())', () =>
+    {
+        test('Given a JS Date() object, should return YYYY-MM-DD', () =>
+        {
+            expect(getDateStr(testDate)).toBe(expectedDate);
+        });
+
+        test('Given an insane object, should return an error', () =>
+        {
+            expect(getDateStr(badDate)).not.toBe(expectedDate);
+        });
     });
 
-    test("Given an insane object, should return an error", () => {
-      expect(getDateStr(badDate)).not.toBe(expectedDate);
+    describe('getCurrentDateTimeStr()', () =>
+    {
+        const regexCurrentDateTime = /(\d{4}_(0[1-9]|1[0-2])_(0[1-9]|[12]\d|3[01])_(0\d|1\d|2[0-3])_([0-5]\d)_([0-5]\d))/g;
+        test('Should return Current Date Time string in YYYY_MM_DD_HH_MM_SS format with no spaces or unexpected characters', () =>
+        {
+            expect(regexCurrentDateTime.test(getCurrentDateTimeStr())).toBe(true);
+        });
     });
-  });
-
-  describe("getCurrentDateTimeStr()", () => {
-    const regexCurrentDateTime = /(\d{4}_(0[1-9]|1[0-2])_(0[1-9]|[12]\d|3[01])_(0\d|1\d|2[0-3])_([0-5]\d)_([0-5]\d))/g;
-    test("Should return Current Date Time string in YYYY_MM_DD_HH_MM_SS format with no spaces or unexpected characters", () => {
-      expect(regexCurrentDateTime.test(getCurrentDateTimeStr())).toBe(true);
-    });
-  });
 });
