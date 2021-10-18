16539033e24c7f4613a459125ae39f57959af0b3
fcoiuri <fcoiuri@gmail.com>
2021-10-17 23:21:26 -0300

    removing white spaces and empty lines

--- __tests__/__main__/date-aux.js
+++ __tests__/__main__/date-aux.js
@@ -8,21 +8,20 @@ describe('Date Functions', () =>
     const badDate = ['this', 'is', 'not', 'a', 'date'];
     const testDate = new Date();
     const expectedDate = new Date(testDate.getTime() - (testDate.getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
-    
+
     describe('getDateStr(Date())', () =>
     {
         test('Given a JS Date() object, should return YYYY-MM-DD', () =>
         {
             expect(getDateStr(testDate)).toBe(expectedDate);
         });
-        
+
         test('Given an insane object, should return an error', () =>
         {
             expect(getDateStr(badDate)).not.toBe(expectedDate);
         });
-        
     });
-    
+
     describe('getCurrentDateTimeStr()', () =>
     {
         const regFullDate = /[0-9]{4}_(0[1-9]|1[0-2])_(0[1-9]|[1-2][0-9]|3[0-1])_(2[0-3]|[01][0-9])_[0-5][0-9]_[0-5][0-9]/g;
@@ -32,5 +31,4 @@ describe('Date Functions', () =>
             expect(testFormatNoSpace).toBeTruthy();
         });
     });
-
 });
