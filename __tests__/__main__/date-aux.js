5008f791d6c73844f06c20b6efaa4d975200409e
Vamsi Charan Adari <adarivamsi@gmail.com>
2021-10-18 22:59:38 -0500

    Added requested changes.

--- __tests__/__main__/date-aux.js
+++ __tests__/__main__/date-aux.js
@@ -8,6 +8,7 @@ describe('Date Functions', () =>
     const badDate = ['this', 'is', 'not', 'a', 'date'];
     const testDate = new Date();
     const expectedDate = new Date(testDate.getTime() - (testDate.getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
+    const looseRegexCurrentDateTime = /(\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2})/g;
     const regexCurrentDateTime = /(\d{4}_(0[1-9]|1[0-2])_(0[1-9]|[12]\d|3[01])_(0\d|1\d|2[0-3])_([0-5]\d)_([0-5]\d))/g;
 
     describe('getDateStr(Date())', () =>
@@ -25,6 +26,11 @@ describe('Date Functions', () =>
 
     describe('getCurrentDateTimeStr()', () =>
     {
+        test('Should return Current Date Time string in YYYY_MM_DD_HH_MM_SS format with no spaces or unexpected characters making sure it accepts digits', () =>
+        {
+            expect(looseRegexCurrentDateTime.test(getCurrentDateTimeStr())).toBe(true);
+        });
+
         test('Should return Current Date Time string in YYYY_MM_DD_HH_MM_SS format with no spaces or unexpected characters', () =>
         {
             expect(regexCurrentDateTime.test(getCurrentDateTimeStr())).toBe(true);
