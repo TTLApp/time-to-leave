a12db754291f43e0c0625eb37d77b7535928f8ba
Vamsi Charan Adari <adarivamsi@gmail.com>
2021-10-18 23:19:57 -0500

    Pushing constants to inside the scope.

--- __tests__/__main__/date-aux.js
+++ __tests__/__main__/date-aux.js
@@ -8,8 +8,6 @@ describe('Date Functions', () =>
     const badDate = ['this', 'is', 'not', 'a', 'date'];
     const testDate = new Date();
     const expectedDate = new Date(testDate.getTime() - (testDate.getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
-    const looseRegexCurrentDateTime = /(\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2})/g;
-    const regexCurrentDateTime = /(\d{4}_(0[1-9]|1[0-2])_(0[1-9]|[12]\d|3[01])_(0\d|1\d|2[0-3])_([0-5]\d)_([0-5]\d))/g;
 
     describe('getDateStr(Date())', () =>
     {
@@ -26,6 +24,9 @@ describe('Date Functions', () =>
 
     describe('getCurrentDateTimeStr()', () =>
     {
+        const looseRegexCurrentDateTime = /(\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2})/g;
+        const regexCurrentDateTime = /(\d{4}_(0[1-9]|1[0-2])_(0[1-9]|[12]\d|3[01])_(0\d|1\d|2[0-3])_([0-5]\d)_([0-5]\d))/g;
+
         test('Should return Current Date Time string in YYYY_MM_DD_HH_MM_SS format with no spaces or unexpected characters making sure it accepts digits', () =>
         {
             expect(looseRegexCurrentDateTime.test(getCurrentDateTimeStr())).toBe(true);
