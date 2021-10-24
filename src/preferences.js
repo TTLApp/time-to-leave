TTLApp/time-to-leave

commit 81df5ba98ea92696d503768ea2208c97653b6ced

Adapting Preferences window to use a non-remote environment (#786)

src/preferences.js
@@ -1,48 +1,23 @@
 'use strict';
 
-const { getUserPreferences } = require('../js/user-preferences.js');
-const { applyTheme } = require('../js/themes.js');
-const { bindDevToolsShortcut } = require('../js/window-aux.js');
-const i18n = require('../src/configs/i18next.config');
-const config = require('../src/configs/app.config');
-
-const $ = require('jquery');
-const jqueryI18next = require('jquery-i18next');
-
-// Lazy loaded modules
-let ipcRenderer = null;
-function getIpcRenderer()
-{
-    if (ipcRenderer === null)
-    {
-        ipcRenderer = require('electron').ipcRenderer;
-    }
-    return ipcRenderer;
-}
+import { applyTheme } from '../renderer/themes.js';
+import { translatePage } from '../renderer/i18n-translator.js';
+import userPreferences from '../js/user-preferences.js';
 
 // Global values for preferences page
-let usersStyles = getUserPreferences();
-const preferences = usersStyles;
+let usersStyles;
+let preferences;
 
-function translatePage(language)
-{
-    $('html').attr('lang', language);
-    $('body').localize();
-    $('title').localize();
-    $('label').localize();
-    $('div').localize();
-}
-
-function populateLanguages(i18n)
+function populateLanguages()
 {
     const languageOpts = $('#language');
     languageOpts.empty();
-    $.each(config.getLanguagesCodes(), function()
+    $.each(window.mainApi.getLanguageMap(), (key, value) =>
     {
         languageOpts.append(
             $('<option />')
-                .val(this)
-                .text(config.getLanguageName(this))
+                .val(key)
+                .text(value)
         );
     });
     // Select current display language
@@ -51,54 +26,51 @@ function populateLanguages(i18n)
     {
         $('#language').val(usersStyles['language']);
     }
-    $('#language').on('change', function()
-    {
-        preferences['language'] = this.value;
-        i18n.changeLanguage(this.value);
-        translatePage(this.value);
-        getIpcRenderer().send('PREFERENCE_SAVE_DATA_NEEDED', preferences);
-    });
 }
 
 function listenerLanguage()
 {
     $('#language').on('change', function()
     {
-        console.log('PREFERENCE_SAVE_DATA_NEEDED');
         preferences['language'] = this.value;
-        i18n.changeLanguage(this.value);
-        translatePage(this.value);
-        populateLanguages(i18n);
-        getIpcRenderer().send('PREFERENCE_SAVE_DATA_NEEDED', preferences);
+        window.mainApi.changeLanguage(this.value).then(() =>
+        {
+            translatePage(this.value);
+            window.mainApi.notifyNewPreferences(preferences);
+        });
     });
 }
 
-i18n.on('loaded', () =>
+function handlei18nLoadPromise()
 {
-    i18n.changeLanguage(usersStyles['language']);
-    populateLanguages(i18n);
-    listenerLanguage();
-    i18n.off('loaded');
-    i18n.off('languageChanged');
-
-    jqueryI18next.init(i18n, $);
-    translatePage(i18n.language);
-});
+    window.mainApi.i18nLoadedPromise.then(() =>
+    {
+        window.mainApi.changeLanguage(usersStyles['language']).then(() =>
+        {
+            populateLanguages();
+            listenerLanguage();
+            translatePage(usersStyles['language']);
+        });
+    });
+}
 
 function refreshContent()
 {
-    usersStyles = getUserPreferences();
-}
-
-function updateUserPreferences()
-{
-    getIpcRenderer().send('PREFERENCE_SAVE_DATA_NEEDED', preferences);
+    return new Promise((resolve) =>
+    {
+        window.mainApi.getUserPreferencesPromise().then(userPreferences =>
+        {
+            usersStyles = userPreferences;
+            preferences = usersStyles;
+            resolve();
+        });
+    });
 }
 
 function changeValue(type, newVal)
 {
     preferences[type] = newVal;
-    updateUserPreferences();
+    window.mainApi.notifyNewPreferences(preferences);
 }
 
 function renderPreferencesWindow()
@@ -214,16 +186,20 @@ function renderPreferencesWindow()
     {
         notificationsInterval.prop('disabled', !repetition.is(':checked'));
     });
-
-    bindDevToolsShortcut(window);
 }
 /* istanbul ignore next */
 $(() =>
 {
-    renderPreferencesWindow();
+    window.mainApi.getUserPreferencesPromise().then((userPreferences) =>
+    {
+        usersStyles = userPreferences;
+        preferences = usersStyles;
+        renderPreferencesWindow();
+        handlei18nLoadPromise();
+    });
 });
 
-module.exports = {
+export {
     refreshContent,
     populateLanguages,
     listenerLanguage,

__tests__/__renderer__/preferences.js
@@ -6,12 +6,20 @@ const path = require('path');
 const {
     defaultPreferences,
     getPreferencesFilePath,
+    getUserPreferences,
     savePreferences
 } = require('../../js/user-preferences');
+const { preferencesApi } = require('../../renderer/preload-scripts/preferences-api.js');
 const i18n = require('../../src/configs/i18next.config');
 
 /* eslint-disable-next-line no-global-assign */
 window.$ = require('jquery');
+
+// APIs from the preload script of the preferences window
+window.mainApi = preferencesApi;
+// Mocking with the actual access that main would have
+window.mainApi.getUserPreferencesPromise = () => { return new Promise((resolve) => resolve(getUserPreferences()));}
+
 const {
     refreshContent,
     populateLanguages,
@@ -86,17 +94,18 @@ describe('Test Preferences Window', () =>
 
     describe('Changing values of items in window', () =>
     {
-        beforeEach(() =>
+        beforeEach((async (done) =>
         {
             prepareMockup();
+            await refreshContent();
             renderPreferencesWindow();
             populateLanguages(i18n);
             listenerLanguage();
-        });
+            done();
+        }));
         afterEach(() =>
         {
             savePreferences(testPreferences);
-            refreshContent();
         });
         test('Change count-today to true', () =>
         {

__tests__/__main__/themes.js
@@ -1,0 +1,29 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const {
+    isValidTheme
+} = require('../../js/themes.js');
+
+describe('Main: Theme Functions', function()
+{
+    describe('isValidTheme()', function()
+    {
+        test('should validate', () =>
+        {
+            expect(isValidTheme('system-default')).toBeTruthy();
+            expect(isValidTheme('light')).toBeTruthy();
+            expect(isValidTheme('dark')).toBeTruthy();
+            expect(isValidTheme('cadent-star')).toBeTruthy();
+        });
+    });
+
+    describe('isValidTheme()', function()
+    {
+        test('should not validate', () =>
+        {
+            expect(isValidTheme('foo')).not.toBeTruthy();
+            expect(isValidTheme('bar')).not.toBeTruthy();
+        });
+    });
+});

__tests__/__renderer__/themes.js
@@ -1,15 +1,14 @@
 /* eslint-disable no-undef */
 'use strict';
 
-const {
+import {
     applyTheme,
     isValidTheme
-} = require('../../js/themes');
+} from '../../renderer/themes.js';
 window.$ = window.jQuery = require('jquery');
 
 describe('Theme Functions', function()
 {
-
     describe('isValidTheme()', function()
     {
         test('should validate', () =>

index.html
@@ -15,7 +15,7 @@
     <script>window.$ = window.jQuery = require('jquery');</script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js" charset="utf-8"></script>
     <script src="node_modules/jquery-mousewheel/jquery.mousewheel.js"></script>
-    <script src="js/calendar.js" charset="utf-8"></script>
+    <script type="module" src="js/calendar.js" charset="utf-8"></script>
 </head>
 
 <body>

js/calendar.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { applyTheme } from '../renderer/themes.js';
+
 const { ipcRenderer } = require('electron');
 const {
     subtractTime,
@@ -14,7 +16,6 @@ const {
     notificationIsEnabled,
     repetitionIsEnabled
 } = require('./js/user-preferences.js');
-const { applyTheme } = require('./js/themes.js');
 const { CalendarFactory } = require('./js/classes/CalendarFactory.js');
 const { getDateStr } = require('./js/date-aux.js');
 const i18n = require('./src/configs/i18next.config.js');

js/menus.js
@@ -125,8 +125,9 @@ function getEditMenuTemplate(mainWindow)
                     resizable: true,
                     icon: appConfig.iconpath,
                     webPreferences: {
-                        enableRemoteModule: true,
-                        nodeIntegration: true
+                        nodeIntegration: true,
+                        preload: path.join(__dirname, '../renderer/preload-scripts/preferences-bridge.js'),
+                        contextIsolation: true
                     } });
                 prefWindow.setMenu(null);
                 prefWindow.loadURL(htmlPath);
@@ -139,6 +140,13 @@ function getEditMenuTemplate(mainWindow)
                     {
                         savePreferences(savedPreferences);
                         mainWindow.webContents.send('PREFERENCE_SAVED', savedPreferences);
+                    }
+                });
+                prefWindow.webContents.on('before-input-event', (event, input) =>
+                {
+                    if (input.control && input.shift && input.key.toLowerCase() === 'i')
+                    {
+                        BrowserWindow.getFocusedWindow().webContents.toggleDevTools();
                     }
                 });
             },

js/themes.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// TODO: this is duplicated in renderer/themes.js and in preferences.html.
+// Please concentrate it in a single place, probably a JSON.
 const themeOptions = ['system-default', 'light', 'dark', 'cadent-star'];
 
 /**
@@ -12,29 +14,6 @@ function isValidTheme(testTheme)
     return themeOptions.indexOf(testTheme) >= 0;
 }
 
-/**
- * Takes the provided theme key, and loads into a data-attribute on the DOM
- * @param {string} theme
- * @return {boolean} If the theme application was successful
- * */
-function applyTheme(theme)
-{
-    if (isValidTheme(theme) === false)
-    {
-        return false;
-    }
-
-    if (theme === 'system-default')
-    {
-        theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    }
-
-    // Applies to the Primary view
-    $('html').attr('data-theme', theme);
-    return true;
-}
-
 module.exports = {
-    isValidTheme,
-    applyTheme
+    isValidTheme
 };

commit 5698830dd9340d85efb0ea99660789e0684468ad

Broken preferences window after #786

src/preferences.js
@@ -2,7 +2,6 @@
 
 import { applyTheme } from '../renderer/themes.js';
 import { translatePage } from '../renderer/i18n-translator.js';
-import userPreferences from '../js/user-preferences.js';
 
 // Global values for preferences page
 let usersStyles;