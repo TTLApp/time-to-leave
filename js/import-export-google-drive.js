9ec0781ad8ef448769a4a831b08fce772a28b189
ge72mum <ge72mum@mytum.de>
2023-01-05 08:35:33 +0100

    simplify export function

--- js/import-export-google-drive.js
+++ js/import-export-google-drive.js
@@ -13,24 +13,28 @@ const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
  *
  * @return {Promise<OAuth2Client|null>}
  */
-async function loadSavedCredentialsIfExist() {
-    try {
+async function loadSavedCredentialsIfExist()
+{
+    try
+    {
         const content = await fs.readFile(TOKEN_PATH);
         const credentials = JSON.parse(content);
         return google.auth.fromJSON(credentials);
     }
-    catch (err) {
+    catch (err)
+    {
         return null;
     }
 }
 
 /**
- * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
  *
  * @param {OAuth2Client} client
  * @return {Promise<void>}
  */
-async function saveCredentials(client) {
+async function saveCredentials(client)
+{
     const content = await fs.readFile(CREDENTIALS_PATH);
     const keys = JSON.parse(content);
     const key = keys.installed || keys.web;
@@ -46,60 +50,62 @@ async function saveCredentials(client) {
 /**
  * Load or request or authorization to call APIs.
  */
-async function authorize() {
+async function authorize()
+{
     let client = await loadSavedCredentialsIfExist();
-    if (client) {
+    if (client)
+    {
         return client;
     }
     client = await authenticate({
         scopes: SCOPES,
         keyfilePath: CREDENTIALS_PATH,
     });
-    if (client.credentials) {
+    if (client.credentials)
+    {
         await saveCredentials(client);
     }
     return client;
 }
 
 /**
- * Upload Db content as JSON to Google Drive
+ * Upload database content as JSON to Google Drive
  * @param {OAuth2Client} authClient An authorized OAuth2 client.
  * @param {String} path Path and name of the uploaded file
  */
-async function uploadData(authClient, path) {
-    // TODO: can/ should this be done without creating a tmp file?
-    const fs = require('fs');
+async function uploadData(authClient, path)
+{
     const service = google.drive({ version: 'v3', auth: authClient });
-    fs.writeFileSync('tmp', getDbAsJSON());
+    const jsonData = getDbAsJSON();
     const fileMetadata = {
         name: path,
         mimeType: 'application/json',
     };
     const media = {
         mimeType: 'application/json',
-        body: fs.createReadStream('tmp'),
+        body: jsonData,
     };
-    try {
+    try
+    {
         await service.files.create({
             resource: fileMetadata,
             media: media,
-            fields: 'id',
         });
-        fs.unlinkSync('tmp');
     }
-    catch (err) {
+    catch (err)
+    {
         throw new Error('Failed to upload data.');
     }
 }
 
 /**
- * Authorize and upload Db content as JSON to Google Drive
+ * Authorize and upload database content as JSON to Google Drive
  * @param {String} path Path and name of the uploaded file
  */
-async function exportDatabaseToGoogleDrive(path) {
+async function exportDatabaseToGoogleDrive(path)
+{
     const client = await authorize();
     await uploadData(client, path);
-
 }
 
 
@@ -107,10 +113,12 @@ async function exportDatabaseToGoogleDrive(path) {
  * Search file in drive location
  * @return {String} fileId
  * */
-async function searchFile(authClient, fileName) {
+async function searchFile(authClient, fileName)
+{
     const service = google.drive({ version: 'v3', auth: authClient });
     const files = [];
-    try {
+    try
+    {
         const res = await service.files.list({
             q: 'name=\'' + fileName + '\'',
             fields: 'nextPageToken, files(id, name)',
@@ -122,7 +130,8 @@ async function searchFile(authClient, fileName) {
         const fileId = res.data.files[0].id;
         return fileId;
     }
-    catch (err) {
+    catch (err)
+    {
         throw new Error('Failed to find file ' + fileName + ' in Drive.');
     }
 }
@@ -132,9 +141,11 @@ async function searchFile(authClient, fileName) {
  * @param {OAuth2Client} authClient An authorized OAuth2 client.
  * @param {String} fileId ID of the file that should be downloaded.
  */
-async function downloadFile(authClient, fileId) {
+async function downloadFile(authClient, fileId)
+{
     const service = google.drive({ version: 'v3', auth: authClient });
-    try {
+    try
+    {
         const file = await service.files.get({
             fileId: fileId,
             alt: 'media',
@@ -142,12 +153,14 @@ async function downloadFile(authClient, fileId) {
         const json_data = JSON.stringify(file.data, null, '\t');
         return json_data;
     }
-    catch (err) {
+    catch (err)
+    {
         throw new Error('Failed to download file.');
     }
 }
 
-async function importDatabaseFromGoogleDrive() {
+async function importDatabaseFromGoogleDrive()
+{
     // TODO: check if the downloaded file has TTL data and the right format
     // TODO: file name hardcoded at the moment, add user input
     const client = await authorize();
