262f03882f570a956d9ab6a4ffb7684d69e45ac9
Arthur Araujo <araujoarthur0@hotmail.com>
2024-01-01 22:32:21 -0300

    Mocking and checking two expected console.log scenarios

--- __tests__/__renderer__/classes/BaseCalendar.js
+++ __tests__/__renderer__/classes/BaseCalendar.js
@@ -167,14 +167,22 @@ describe('BaseCalendar.js', () =>
             expect(mocks.compute).toHaveBeenCalledTimes(0);
         });
 
-        test('Should not update value because of rejection', async() =>
+        test('Should not update value because of rejection', async(done) =>
         {
+            mocks.consoleLog = jest.spyOn(console, 'log').mockImplementation();
             mocks.compute = jest.spyOn(timeBalance, 'computeAllTimeBalanceUntilAsync').mockImplementation(() => Promise.reject());
             const preferences = {view: 'day'};
             const languageData = {hello: 'hola'};
             const calendar = new ExtendedClass(preferences, languageData);
             calendar._updateAllTimeBalance();
             expect(mocks.compute).toHaveBeenCalledTimes(1);
+
+            // When the rejection happens, we call console.log, but it'll be undefined here
+            setTimeout(() =>
+            {
+                expect(mocks.consoleLog).toHaveBeenCalledWith(undefined);
+                done();
+            }, 500);
         });
 
         test('Should not update value because no overall-balance element', () =>
