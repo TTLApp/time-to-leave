7ff4c217e93529544ae34011be1c61b6001a655d
Arthur Araujo <araujoarthur0@hotmail.com>
2025-02-24 10:14:15 -0300

    Adding a calendar window test

--- src/calendar.js
+++ src/calendar.js
@@ -16,79 +16,65 @@ function setupCalendar(preferences)
     });
 }
 
-/*
- * Reload the calendar upon request from main
- */
-window.calendarApi.handleCalendarReload(async() =>
+function setupIpcHandlers()
 {
-    await calendar.reload();
-});
-
-/*
- * Update the calendar after a day has passed
- */
-window.calendarApi.handleRefreshOnDayChange((event, oldDate, oldMonth, oldYear) =>
-{
-    calendar.refreshOnDayChange(oldDate, oldMonth, oldYear);
-});
-
-/*
- * Get notified when preferences has been updated.
- */
-window.calendarApi.handlePreferencesSaved((event, prefs) =>
-{
-    setupCalendar(prefs);
-});
-
-/*
- * Get notified when waivers get updated.
- */
-window.calendarApi.handleWaiverSaved(async() =>
-{
-    await calendar.loadInternalWaiveStore();
-    calendar.redraw();
-});
-
-/*
- * Punch the date and time as requested by user.
- */
-window.calendarApi.handlePunchDate(() =>
-{
-    calendar.punchDate();
-});
-
-/*
- * Reload theme.
- */
-window.calendarApi.handleThemeChange(async(event, theme) =>
-{
-    applyTheme(theme);
-});
-
-/*
- * Returns value of "leave by" for notifications.
- */
-window.calendarApi.handleLeaveBy(searchLeaveByElement);
-
-/*
- * Change the main window style to indicate an operation is processing.
- */
-window.calendarApi.handleToggleMainWindowWait((event, enable) =>
-{
-    console.log(enable);
-    const waitClass = 'wait';
-    if (enable)
+    // Reload the calendar upon request from main
+    window.calendarApi.handleCalendarReload(async() =>
     {
-        if (!$('html').hasClass(waitClass))
+        await calendar.reload();
+    });
+
+    // Update the calendar after a day has passed
+    window.calendarApi.handleRefreshOnDayChange((event, oldDate, oldMonth, oldYear) =>
+    {
+        calendar.refreshOnDayChange(oldDate, oldMonth, oldYear);
+    });
+
+    // Get notified when preferences has been updated.
+    window.calendarApi.handlePreferencesSaved((event, prefs) =>
+    {
+        setupCalendar(prefs);
+    });
+
+    // Get notified when waivers get updated.
+    window.calendarApi.handleWaiverSaved(async() =>
+    {
+        await calendar.loadInternalWaiveStore();
+        calendar.redraw();
+    });
+
+    // Punch the date and time as requested by user.
+    window.calendarApi.handlePunchDate(() =>
+    {
+        calendar.punchDate();
+    });
+
+    // Reload theme.
+    window.calendarApi.handleThemeChange(async(event, theme) =>
+    {
+        applyTheme(theme);
+    });
+
+    // Returns value of "leave by" for notifications.
+    window.calendarApi.handleLeaveBy(searchLeaveByElement);
+
+    // Change the main window style to indicate an operation is processing.
+    window.calendarApi.handleToggleMainWindowWait((event, enable) =>
+    {
+        const waitClass = 'wait';
+        if (enable)
         {
-            $('html').addClass(waitClass);
+            if (!$('html').hasClass(waitClass))
+            {
+                $('html').addClass(waitClass);
+            }
         }
-    }
-    else
-    {
-        $('html').removeClass(waitClass);
-    }
-});
+        else
+        {
+            $('html').removeClass(waitClass);
+        }
+    });
+}
 
 // On page load, create the calendar and setup notification
 $(() =>
@@ -105,4 +91,5 @@ $(() =>
             }, 100);
         });
     });
+    setupIpcHandlers();
 });
