9a1a49f927b72ab1d027a5c693c1b569d3aee7fd
Casey Parker <02-germane-atomic@icloud.com>
2025-06-06 10:00:33 -0700

    modified:   renderer/notification-channel.js Call with window.electronAPI, because renderer 	modified:   renderer/preload-scripts/calendar-bridge.mjs Expose electronAPI.sendLeaveBy for the renderer

--- renderer/notification-channel.js
+++ renderer/notification-channel.js
@@ -5,7 +5,7 @@ import IpcConstants from '../js/ipc-constants.mjs';
 const searchLeaveByElement = (event) =>
 {
     const leaveByElement = $('#leave-by').val();
-    event.sender.send(IpcConstants.ReceiveLeaveBy, leaveByElement);
+    window.electronAPI.sendLeaveBy(IpcConstants.ReceiveLeaveBy, leaveByElement);
 };
 
 export {
