2bf0da20e39b88a050f6a3ef4b4d259d159d19bb
Tulio Leao <tupaschoal@gmail.com>
2026-08-01 08:23:16 -0300

    Update jQuery import method in mock file
    Changed the way jQuery is imported to use 'require' instead of 'import'.

--- __mocks__/jquery.mjs
+++ __mocks__/jquery.mjs
@@ -1,8 +1,10 @@
 // Helper file sets global window and $ for renderer imports
+import { createRequire } from 'module';
 import jsdom from 'jsdom';
 
+const require = createRequire(import.meta.url);
 global.window = new jsdom.JSDOM().window;
-window.$ = global.$ = (await import('jquery')).default;
+window.$ = global.$ = require('jquery');
 
 // Mocking matchMedia since it's not usually defined but we use it
 window.matchMedia = window.matchMedia || function()
